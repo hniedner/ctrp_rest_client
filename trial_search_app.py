7d43df8d861b73ffb7c5e11531f3848a248bcc6a
R. Hannes Niedner <hniedner@gmail.com>
2017-02-06 21:53:55 -0800

    Correct redirect after search from submission

--- trial_search_app.py
+++ trial_search_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_nav import Nav
 
@@ -29,7 +29,7 @@ def home():
     return search_form()
 
 
-@app.route('/display_trial')
+@app.route('/display_trial/<string:nct_id>')
 def display_trial(nct_id):
     # retrieving trial as dictionary from the CTRP API client
     trial_dict = api_client.get_trial_by_nct_id(nct_id)
@@ -42,11 +42,10 @@ def display_trial(nct_id):
 def search_form():
     form = SimpleSearchForm()
     if form.validate_on_submit():
-        return display_trial(form.nct_id.data)
+        return redirect(url_for('display_trial', nct_id=form.nct_id.data))
 
     # Render template
     return render_template('search_form.html', form=form)
-
 
 
 # Run Flask webapp
