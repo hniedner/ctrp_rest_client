d717f0e1e35815327a3dc3778dc801bf5edb71c5
R. Hannes Niedner <hniedner@gmail.com>
2017-02-10 21:41:43 -0800

    Moved simple trial search by trial id to the navigation bar. Replaced flask-nav with hand written navbar in base.html to accommodate the form.

--- ctrp_rest_client/__init__.py
+++ ctrp_rest_client/__init__.py
@@ -1,18 +1,17 @@
 import json
 
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
-from flask_nav import Nav
-from flask_nav.elements import Navbar, View
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 from ctrp_rest_client.static import api_client
 
 
 class SimpleSearchForm(FlaskForm):
-    nct_id = StringField('nct_id', validators=[DataRequired()])
+    nct_id = StringField('NCT or NCI Trial ID', validators=[DataRequired()])
+    submit = SubmitField("Display")
 
 
 # Initialize the Flask application
@@ -23,24 +22,16 @@ app.debug = True
 # Install our Bootstrap extension
 Bootstrap(app)
 
-# We initialize the navigation as well
-nav = Nav()
 
-
-@nav.navigation()
-def top_navbar():
-    return Navbar(
-        'NCI Clinical Trial Search', View('Home', 'home'),
-    )
-nav.init_app(app)
-
-
+# home page - redirecting straight to search form
 @app.route('/')
 def home():
-    return search_form()
+    # Render template
+    return render_template('home.html')
 
 
-@app.route('/display_trial/<string:nct_id>')
+# display information for the trial identified by the nct_id (or nci id)
+@app.route('/display_trial/<string:nct_id>', methods=['GET'])
 def display_trial(nct_id):
     # retrieving trial as dictionary from the CTRP API client
     trial_dict = api_client.get_trial_by_nct_id(nct_id)
@@ -49,6 +40,18 @@ def display_trial(nct_id):
     return render_template('display_trial.html', trial=trial_dict)
 
 
+# display information for the trial identified by the nct_id (or nci id)
+@app.route('/show_trial', methods=['POST'])
+def show_trial():
+    # parse form parameter
+    trial_id = request.form["trial_id"]
+    # retrieving trial as dictionary from the CTRP API client
+    trial_dict = api_client.get_trial_by_nct_id(trial_id)
+    # Render template
+    return render_template('display_trial.html', trial=trial_dict)
+
+
+# display search form
 @app.route('/search_form', methods=('GET', 'POST'))
 def search_form():
     form = SimpleSearchForm()
@@ -59,12 +62,14 @@ def search_form():
     return render_template('search_form.html', form=form)
 
 
+# display datatable with search results
 @app.route('/display_results')
 def display_results():
     # Render template
     return render_template('display_results.html')
 
 
+# ajax callback to retrieve search results
 @app.route('/_get_data')
 def _get_data():
     search_params = {
