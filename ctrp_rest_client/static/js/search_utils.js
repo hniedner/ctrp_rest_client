6e7e6423a38963916aafc68675d3c716265a5b0b
R. Hannes Niedner <hannes.niedner@ucsf.edu>
2017-02-24 18:01:26 -0800

    resolved issue: creation of duplicate search term through expansion refactored javascript function even more

--- ctrp_rest_client/static/js/search_utils.js
+++ ctrp_rest_client/static/js/search_utils.js
@@ -4,22 +4,35 @@ function tag_id(id_name) {
     return '#' + id_name;
 };
 
+// translate domain (search category such as disease, biomarker, etc.)
+// into standardized field ids following the convention:
+// disease ->
+//      disease_names -> for hidden field holding the concept names as comma delimited string
+//      disease_codes -> for hidden field holding the concept codes as comma delimited string
+//      diseases      -> for unordered list of selected concept names
+function get_fields(dom) {
+
+    return {
+        'name_list': tag_id(dom + '_names'),
+        'code_list': tag_id(dom + '_codes'),
+        'ul_list': tag_id(dom + 's')
+    }
+}
+
 // load comma delimited list of concept codes from hidden string field and populate an UL
 // will callback to retrieve names for the concept codes
 // code_list_id - id of the text form field
 // ul_list_id - id of the ul list
 function refresh_ul_list_from_hidden(dom) {
 
-    var code_list_id = dom + '_codes';
-    var name_list_id = dom + '_names';
-    var ul_list_id = dom + 's';
-    var names = $(tag_id(name_list_id)).val().split(', ');
-    var codes = $(tag_id(code_list_id)).val().split(', ');
+    var fields = get_fields(dom);
+    var names = $(fields.name_list).val().split(', ');
+    var codes = $(fields.code_list).val().split(', ');
 
     $.each(names, function (i, name) {
         if (name != '') {
             var code = codes[i];
-            $(tag_id(ul_list_id)).append(create_li(code, name, dom));
+            $(fields.ul_list).append(create_li(code, name, dom));
         }
     });
 };
@@ -27,14 +40,13 @@ function refresh_ul_list_from_hidden(dom) {
 // wrapper to call the remove_term for both names and code hidden fields
 function rm_term(code, name, dom) {
 
-    var name_list_id = dom + '_names';
-    var code_list_id = dom + '_codes';
+    var fields = get_fields(dom);
     // remove ul list item
     $(tag_id(code)).remove();
     // remove name from hidden form field
-    remove_term(name, name_list_id);
+    remove_term(name, fields.name_list);
     // remove code from hidden form field
-    remove_term(code, code_list_id);
+    remove_term(code, fields.code_list);
 };
 
 // remove term from comma separated list in text field
@@ -51,27 +63,15 @@ function remove_term(term, list_id) {
 // dom is domain either biomarker or disease (or other in the future)
 function expand_term(code, dom) {
 
-    var name_list_id = dom + '_names';
-    var code_list_id = dom + '_codes';
-    var ul_list_id = dom + 's';
-
     $.get('expand_ncit_code?code=' + code, function (data) {
         data.forEach(function(entry) {
 
             var code = entry.data;
             var name = entry.value.replace('_',' ');
-
-            // TODO: creates dublicate entries - Fix!
-            $(tag_id(code_list_id)).val(
-                $.grep([$(tag_id(code_list_id)).val(), code], Boolean).join(", ") );
-
-            $(tag_id(name_list_id)).val(
-                $.grep([$(tag_id(name_list_id)).val(), name], Boolean).join(", ") );
-
-            $(tag_id(ul_list_id)).append( create_li(entry.data, name, dom) );
+            update_selections(code, name, dom);
         });
     });
-}
+};
 
 
 // generates link for selected code and name
@@ -84,24 +84,15 @@ function create_li(code, name, dom) {
     + '&nbsp; ' + name + '</li>';
 };
 
-// updates hidden code list and visible list of names fore
-// terms selected in typeahead field search
-function update_typeahead(item, dom) {
+// update the hidden fields for codes and names
+// and the unordered list for selected terms
+function update_selections(code, name, dom) {
 
-    var name_list_id = dom + '_names';
-    var code_list_id = dom + '_codes';
-    var ul_list_id = dom + 's';
-    var name_list = tag_id(name_list_id);
-    var code_list = tag_id(code_list_id);
-    var ul_list = tag_id(ul_list_id);
-    var code = map[item].data;
+    var fields = get_fields(dom);
 
-    if(!$(code_list).val().includes(code)) {
-
-        $(name_list).val( $.grep([$(name_list).val(), item], Boolean).join(", ") );
-
-        $(code_list).val( $.grep([$(code_list).val(), code], Boolean).join(", ") );
-
-        $(ul_list).append( create_li(code, item, dom) );
-    }
+    if(!$(fields.code_list).val().includes(code)) {
+        $(fields.name_list).val( $.grep([$(fields.name_list).val(), name], Boolean).join(", ") );
+        $(fields.code_list).val( $.grep([$(fields.code_list).val(), code], Boolean).join(", ") );
+        $(fields.ul_list).append( create_li(code, name, dom) );
+    };
 };
