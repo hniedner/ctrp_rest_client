9f1954a01a3c5e91d0e788b90c249fd9eafd0f6d
R. Hannes Niedner <hniedner@gmail.com>
2017-03-04 09:18:04 -0800

    fixed javascript issue indicated by code climate

--- ctrp_rest_client/static/js/search_utils.js
+++ ctrp_rest_client/static/js/search_utils.js
@@ -2,7 +2,7 @@
 function tag_id(id_name) {
 
     return '#' + id_name;
-};
+}
 
 // translate domain (search category such as disease, biomarker, etc.)
 // into standardized field ids following the convention:
@@ -30,12 +30,12 @@ function refresh_ul_list_from_hidden(dom) {
     var codes = $(fields.code_list).val().split(', ');
 
     $.each(names, function (i, name) {
-        if (name != '') {
+        if (name !== '') {
             var code = codes[i];
             $(fields.ul_list).append(create_li(code, name, dom));
         }
     });
-};
+}
 
 // wrapper to call the remove_term for both names and code hidden fields
 function rm_term(code, name, dom) {
@@ -47,7 +47,7 @@ function rm_term(code, name, dom) {
     remove_term(name, fields.name_list);
     // remove code from hidden form field
     remove_term(code, fields.code_list);
-};
+}
 
 // remove term from comma separated list in text field
 function remove_term(term, list_id) {
@@ -57,7 +57,7 @@ function remove_term(term, list_id) {
     var pos = terms.indexOf(term);
     terms.splice(pos, 1);
     $(list_id).val(terms.join(", "));
-};
+}
 
 function _do_code_callback(code, dom, url, reverse) {
 
@@ -104,12 +104,12 @@ function create_li(code, name, dom) {
 
     return '<li id="' + code + '">'
     + '<a href="#" onclick="rm_term(\'' + code + '\', \'' + name + '\', \'' + dom + '\');">remove</a>'
-    + '&nbsp;|&nbsp;' + '<a href="#" onclick="add_child_terms(\'' + code + '\', \'' + dom + '\');">add child terms</a>'
-    + '&nbsp;|&nbsp;' + '<a href="#" onclick="rm_child_terms(\'' + code + '\', \'' + dom + '\');">remove child terms</a>'
-    + '&nbsp;|&nbsp;' + '<a href="#" onclick="add_parent_terms(\'' + code + '\', \'' + dom + '\');">add parent terms</a>'
-    + '&nbsp;|&nbsp;' + '<a href="#" onclick="rm_parent_terms(\'' + code + '\', \'' + dom + '\');">remove parent terms</a>'
+    + '&nbsp;|&nbsp;<a href="#" onclick="add_child_terms(\'' + code + '\', \'' + dom + '\');">add child terms</a>'
+    + '&nbsp;|&nbsp;<a href="#" onclick="rm_child_terms(\'' + code + '\', \'' + dom + '\');">remove child terms</a>'
+    + '&nbsp;|&nbsp;<a href="#" onclick="add_parent_terms(\'' + code + '\', \'' + dom + '\');">add parent terms</a>'
+    + '&nbsp;|&nbsp;<a href="#" onclick="rm_parent_terms(\'' + code + '\', \'' + dom + '\');">remove parent terms</a>'
     + '&nbsp;|&nbsp;' + name + '</li>';
-};
+}
 
 // update the hidden fields for codes and names
 // and the unordered list for selected terms
@@ -121,5 +121,5 @@ function add_term(code, name, dom) {
         $(fields.name_list).val( $.grep([$(fields.name_list).val(), name], Boolean).join(", ") );
         $(fields.code_list).val( $.grep([$(fields.code_list).val(), code], Boolean).join(", ") );
         $(fields.ul_list).append( create_li(code, name, dom) );
-    };
-};
+    }
+}
