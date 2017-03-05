51db3bb0219f02293bc9cc6670487797a952cd4f
R. Hannes Niedner <hniedner@gmail.com>
2017-03-04 20:16:14 -0800

    'dried' up ajax_callbacks using passing function by name - cool

--- ctrp_rest_client/ajax_callbacks.py
+++ ctrp_rest_client/ajax_callbacks.py
@@ -5,30 +5,22 @@ from ctrp_rest_client import terminology
 
 @app.route('/search_diseases')
 def search_disease():
-    query = request.args.get('q')
-    result = terminology.search_cancertypes_by_substring(query)
-    return jsonify(result)
+    return _process_route('q', terminology.search_cancertypes_by_substring)
 
 
 @app.route('/search_biomarkers')
 def search_biomarkers():
-    query = request.args.get('q')
-    result = terminology.search_biomarkers_by_substring(query)
-    return jsonify(result)
+    return _process_route('q', terminology.search_biomarkers_by_substring)
 
 
 @app.route('/get_child_codes')
 def get_child_codes():
-    code = request.args.get('code')
-    result = terminology.get_child_codes(code)
-    return jsonify(result)
+    return _process_route('code', terminology.get_child_codes)
 
 
 @app.route('/get_parent_codes')
 def get_parent_codes():
-    code = request.args.get('code')
-    result = terminology.get_parent_codes(code)
-    return jsonify(result)
+    return _process_route('code', terminology.get_parent_codes)
 
 
 @app.route('/get_name_for_code')
@@ -37,3 +29,9 @@ def get_name_for_code():
     code = request.args.get('code')
     name = terminology.get_name_for_code(domain, code)
     return jsonify(name)
+
+
+def _process_route(request_param, function_name):
+    value = request.args.get(request_param)
+    result = function_name(value)
+    return jsonify(result)
