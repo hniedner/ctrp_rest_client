9185ed4374d2ec8220f21c4650b122e00a9f4928
R. Hannes Niedner <hniedner@gmail.com>
2017-04-11 14:13:17 -0700

    further dried up the terminology code

--- terminology.py
+++ terminology.py
@@ -16,18 +16,14 @@ def load_terminology_data():
     return data
 
 
-def get_dom_sql(dom):
+def get_sql(query, is_dom):
     global data
     if len(data) == 0:
         load_terminology_data()
-    return data['dom_sql'][dom]
-
-
-def get_sql(query):
-    global data
-    if len(data) == 0:
-        load_terminology_data()
-    return data['sql'][query]
+    if is_dom:
+        return data['dom_sql'][query]
+    else:
+        return data['sql'][query]
 
 
 def get_root_concept(dom):
@@ -93,31 +89,31 @@ def get_single_string_result(sql, querytokens=None):
 
 
 def search_domain_by_substring(dom, query):
-    return get_code_name_list_result(get_dom_sql(dom), ['%' + query + '%', '%' + query + '%'])
+    return get_code_name_list_result(get_sql(dom, True), ['%' + query + '%', '%' + query + '%'])
 
 
 def search_diseases_associated_with_anatomic_site(code):
-    return get_code_name_list_result(get_sql('search_diseases_associated_with_anatomic_site'),
+    return get_code_name_list_result(get_sql('search_diseases_associated_with_anatomic_site', False),
                                      [check_for_root(code, 'anantomicsite')])
 
 
 def search_diseases_associated_with_finding(code):
-    return get_code_name_list_result(get_sql('search_diseases_associated_with_finding'),
+    return get_code_name_list_result(get_sql('search_diseases_associated_with_finding', False),
                                      [check_for_root(code, 'finding')])
 
 
 def search_diseases_associated_with_gene(code):
-    return get_code_name_list_result(get_sql('search_diseases_associated_with_gene'),
+    return get_code_name_list_result(get_sql('search_diseases_associated_with_gene', False),
                                      [check_for_root(code, 'gene')])
 
 
 def search_diseases_is_stage(code):
-    return get_code_name_list_result(get_sql('search_diseases_is_stage'),
+    return get_code_name_list_result(get_sql('search_diseases_is_stage', False),
                                      [check_for_root(code, 'disease')])
 
 
 def search_diseases_is_grade(code):
-    return get_code_name_list_result(get_sql('search_diseases_is_grade'),
+    return get_code_name_list_result(get_sql('search_diseases_is_grade', False),
                                      [check_for_root(code, 'disease')])
 
 
@@ -127,7 +123,7 @@ def get_subtree_codes(code, dom):
     results = []
     try:
         cursor = connection.cursor()
-        cursor.execute(get_sql('get_subtree_codes'), [check_for_root(code, dom)])
+        cursor.execute(get_sql('get_subtree_codes', False), [check_for_root(code, dom)])
         results = cursor.fetchall()
         cursor.close()
     except sqlite3.Error as e:
@@ -141,14 +137,14 @@ def get_child_codes(code, dom):
     # parent codes are in a pipe (|) delimited list
     # just matching on the first term we get spurious substring matches
     # such as C8461 matching C84615
-    return get_code_name_list_result(get_sql('get_child_codes'), ['%' + ccode, '%' + ccode + '|%'])
+    return get_code_name_list_result(get_sql('get_child_codes', False), ['%' + ccode, '%' + ccode + '|%'])
 
 
 def get_parent_codes(code, dom):
     ccode = check_for_root(code, dom)
-    parent_codes = get_single_string_result(get_sql('get_parent_codes'), [ccode])
+    parent_codes = get_single_string_result(get_sql('get_parent_codes', False), [ccode])
     results = []
     # parent codes are in a pipe (|) delimited list
     for parent_code in parent_codes.split('|'):
-        results.extend(get_code_name_list_result(get_sql('get_parent_codes_sub'), [parent_code]))
+        results.extend(get_code_name_list_result(get_sql('get_parent_codes_sub', False), [parent_code]))
     return results
